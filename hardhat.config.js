6be68b9e55861ebbd9d5ceb856c9a66ca2fb6030
Mysticpol <158776762+Mystique85@users.noreply.github.com>
2025-10-04 18:38:24 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 export default {
   solidity: "0.8.20",
   networks: {
     celo: {
       url: "https://forno.celo.org",
-      accounts: ["0xPRIVATE_KEY_HERE"] // <-- podmień na swój klucz prywatny
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
